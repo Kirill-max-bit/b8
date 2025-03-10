906325213704bda48dd77e29e475761e4a0e8b05
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 10:02:24 +0000

    сделал задачу

--- main57.py
+++ main57.py
@@ -1,10 +1,10 @@
 from math import gcd
 
-# Функция для получения всех делителей числа q, взаимно простых с p
+
 def coprime_divisors(q, p):
     divisors = [i for i in range(1, q + 1) if q % i == 0]
     return [d for d in divisors if gcd(d, p) == 1]
 
-# Ввод чисел
+
 q, p = map(int, input("Введите q и p через пробел: ").split())
-print("Делители числа", q, ", взаимно простые с", p, ":", coprime_divisors(q, p))
+print("Делители числа", q, ", взаимно с", p, ":", coprime_divisors(q, p))
