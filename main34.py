cedab92c4adef5759b10fe36e0004f785b2f8424
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 09:14:04 +0000

    сделал задачу

--- main34.py
+++ main34.py
@@ -1,2 +1,9 @@
+# Функция для подсчета суммы делителей числа
+def sum_of_divisors(n):
+    return sum(i for i in range(1, n + 1) if n % i == 0)
+
+
 result = [num for num in range(100, 301) if sum_of_divisors(num) == 50]
+
+# Вывод результата
 print("Числа с суммой делителей, равной 50:", result)
