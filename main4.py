1774e184e934b4de5476f66ef7ce1ed5f65e8fc2
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 06:30:44 +0000

    сделал задачу

---
+++ main4.py
@@ -0,0 +1,22 @@
+for i in range(5):  # Цикл от 0 до 4
+    for j in range(i, -1, -1):  # Внутренний цикл для чисел
+        print(j, end=" ")
+    print()
+
+
+for i in range(6, 2, -1):  # Цикл от 6 до 2
+    for j in range(i, 1, -1):  # Внутренний цикл для чисел
+        print(j, end=" ")
+    print()
+
+
+for i in range(5):  # Цикл от 0 до 4
+    for j in range(30 - i, 31):  # Внутренний цикл для чисел
+        print(j, end=" ")
+    print()
+
+
+for i in range(5):  # Цикл от 0 до 4
+    for j in range(20 - i, 25 - i):  # Внутренний цикл для чисел
+        print(j, end=" ")
+    print()
