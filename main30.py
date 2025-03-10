552884310f4352f4f7ab498d70cc42fbda50fdf1
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 09:08:39 +0000

    сделал задачу

--- main30.py
+++ main30.py
@@ -1,3 +1,8 @@
+# Функция для подсчета количества делителей числа
+def count_divisors(n):
+    return sum(1 for i in range(1, n + 1) if n % i == 0)
+
+
 a, b = map(int, input("Введите a, b через пробел: ").split())
 
 # Находим все числа и их количество делителей
@@ -5,9 +10,11 @@ numbers_with_divisors = [(num, count_divisors(num)) for num in range(a, b + 1)]
 
 # а) Максимальное число с максимальным количеством делителей
 max_divisor_count = max(div for _, div in numbers_with_divisors)
-max_number = max(num for num, div in numbers_with_divisors if div == max_divisor_count)
+max_number = max(num for num, div in numbers_with_divisors if div
+                 == max_divisor_count)
 print("а) Максимальное число:", max_number)
 
 # б) Минимальное число с максимальным количеством делителей
-min_number = min(num for num, div in numbers_with_divisors if div == max_divisor_count)
+min_number = min(num for num, div in numbers_with_divisors if div
+                 == max_divisor_count)
 print("б) Минимальное число:", min_number)
