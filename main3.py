33ae08c6540726f856689111d9c4500552921a9c
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 06:29:53 +0000

    сделал задачу

---
+++ main3.py
@@ -0,0 +1,14 @@
+for i in range(1, 6):  
+    print((str(i) + " ") * i)
+
+
+for i in range(5, 10):  
+    print((str(i) + " ") * (10 - i))
+
+
+for i in range(1, 6):  
+    print((str(i * 10) + " ") * i)
+
+
+for i in range(5, 0, -1):  
+    print((str(i * 5) + " ") * i)
