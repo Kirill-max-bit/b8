9328c00b6ca6b9d25ca2b19cd383e7c39cfb1e78
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 09:15:08 +0000

    сделал задачу

--- main35.py
+++ main35.py
@@ -1,2 +1,9 @@
+# Функция для подсчета суммы делителей числа
+def sum_of_divisors(n):
+    return sum(i for i in range(1, n + 1) if n % i == 0)
+
+
 result = [num for num in range(300, 601) if sum_of_divisors(num) % 10 == 0]
+
+
 print("Числа с суммой делителей, кратной 10:", result)
