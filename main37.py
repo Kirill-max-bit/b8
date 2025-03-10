495446a3a264cca3d979da8f6dc2bbd5a7f7750f
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-10 09:16:10 +0000

    сделал задачу

--- main37.py
+++ main37.py
@@ -1,5 +1,15 @@
+# Функция для подсчета суммы делителей числа
+def sum_of_divisors(n):
+    return sum(i for i in range(1, n + 1) if n % i == 0)
+
+
 a, b = map(int, input("Введите a, b через пробел: ").split())
 
+# Находим максимальную сумму делителей в диапазоне [a, b]
 max_sum = max(sum_of_divisors(num) for num in range(a, b + 1))
+
+# Находим числа с максимальной суммой делителей
 result = [num for num in range(a, b + 1) if sum_of_divisors(num) == max_sum]
+
+# Вывод результата
 print("Числа с максимальной суммой делителей:", result)
